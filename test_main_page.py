ba8582b78d4a4c5fea63e6fe6ff7eac94b69d3f6
Jon Jon <jon@exenple.com>
2021-11-19 14:07:11 +0200

    add @pytest.mark.login_guest to test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,22 +1,41 @@
-from pages.basket_page import BasketPage
+import pytest
+
+from .pages.basket_page import BasketPage
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 import time
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    """ setup — функция, которая выполнится перед запуском каждого теста из класса,
+     обычно туда входит подготовка данных, и teardown — функция, которая выполняется
+     ПОСЛЕ каждого теста из класса, обычно там происходит удаление тех данных, которые
+     мы создали во время теста.
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
+        self.product = ProductFactory(title="Best book created by robot")
+        # создаем по апи
+        self.link = self.product.link
+        yield
+        # после этого ключевого слова начинается teardown
+        # выполнится после каждого теста в классе
+        # удаляем те данные, которые мы создали
+        self.product.delete()"""
+
+    def test_guest_can_go_to_login_page(self, browser):
+        self.link = "http://selenium1py.pythonanywhere.com/"
+        self.page = MainPage(browser, self.link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        self.page.open()                      # открываем страницу
+        self.page.go_to_login_page()
+        self.login_page = LoginPage(browser, browser.current_url)
+        self.login_page.should_be_login_page()
 
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        self.link = "http://selenium1py.pythonanywhere.com/"
+        self.page = MainPage(browser, self.link)
+        self.page.open()
+        self.page.should_be_login_link()
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
